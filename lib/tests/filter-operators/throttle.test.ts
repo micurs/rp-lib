6421fdb8918e20c075b26cfb9694bfdb69254396
Michele Ursino <michele@micurs.com>
2024-12-29 11:08:45 -0800

    Removing unnecessary async in test.

--- lib/tests/filter-operators/throttle.test.ts
+++ lib/tests/filter-operators/throttle.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'jsr:@std/expect';
 import { fromArray, throttle } from '../../src/index.ts';
 
-Deno.test('throttle should emit only one value if the original observable emits all the values in the given time', async () => {
+Deno.test('throttle should emit only one value if the original observable emits all the values in the given time', () => {
   const source$ = fromArray([1, 2, 3, 4, 5]);
   const result$ = throttle<number>(10)(source$);
   const res: Array<number> = [];
