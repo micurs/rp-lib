bdf4e99afeabd5d4aca6a1e679ad9de6e691190f
Michele Ursino <michele@micurs.com>
2024-12-29 11:10:06 -0800

    Micurs/throttle (#9)
    * Adding throttle
    
    * Version update
    
    * Change github action to run our test task.
    
    * Removing unnecessary async in test.

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -10,6 +10,7 @@ export * from './trans-operators/switch-map.ts';
 export * from './trans-operators/concat-map.ts';
 export * from './filter-operators/filter.ts';
 export * from './filter-operators/debounce.ts';
+export * from './filter-operators/throttle.ts';
 export * from './compose-pipe.ts';
 export * from './signals/signal.ts';
 
---
+++ lib/tests/filter-operators/throttle.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'jsr:@std/expect';
+import { fromArray, throttle } from '../../src/index.ts';
+
+Deno.test('throttle should emit only one value if the original observable emits all the values in the given time', () => {
+  const source$ = fromArray([1, 2, 3, 4, 5]);
+  const result$ = throttle<number>(10)(source$);
+  const res: Array<number> = [];
+
+  result$.subscribe({
+    next: (value: number) => {
+      res.push(value);
+    },
+    complete: () => {
+      expect(res).toEqual([1]);
+    },
+  });
+});
